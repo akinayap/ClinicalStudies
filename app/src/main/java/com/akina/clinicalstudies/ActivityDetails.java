2456b148e157ba49fbff62adce65cd0eabea46d1
akinayap <akina.yap@gmail.com>
2018-12-07 10:03:58 +0800

    Updated activity page

--- app/src/main/java/com/akina/clinicalstudies/ActivityDetails.java
+++ app/src/main/java/com/akina/clinicalstudies/ActivityDetails.java
@@ -1,19 +1,29 @@
 package com.akina.clinicalstudies;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ActivityDetails extends AppCompatActivity {
 
     String datatype;
+    final Context context = this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        TextView progress_txt = findViewById(R.id.percentage);
+        ProgressBar progress_bar = findViewById(R.id.progress_circle);
+        progress_txt.setText(progress_bar.getProgress() + "%");
 
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
@@ -40,5 +50,28 @@ public class ActivityDetails extends AppCompatActivity {
                 ActivityDetails.super.finish();
             }
         });
+
+        Button help_btn = findViewById(R.id.help_btn);
+        help_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // custom dialog
+                final Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.popup_dialog);
+
+                Button okButton = dialog.findViewById(R.id.ok_btn);
+                // if button is clicked, close the custom dialog
+                okButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+            }
+        });
+
     }
 }
